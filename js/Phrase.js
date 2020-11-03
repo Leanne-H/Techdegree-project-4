bfde802e89de71eada387e091a894fa2efd7d444
Leanne Heuberger <leanneheuberger@gmail.com>
2020-11-03 14:13:18 +0100

    Updated Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,7 +1,43 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+ /*
+  * "Phrase" is used to handle the creation of phrases.
+ */
+class Phrase {
+     constructor(phrase) {
+       this.phrase = phrase.toLowerCase();
 
+     }
+     /**
+     * Display phrase on game board
+     */
+     addPhraseToDisplay(phrase) {
+       const ul = document.querySelector('[id=phrase] ul');
+       for (let i=0; i < this.phrase.length; i++){
+         let li = document.createElement('li');
+         if (this.phrase[i] === ' '){
+           li.textContent = ' ';
+           li.className = "hide space";
+         } else {
+           li.textContent = `${this.phrase[i]}`;
+           li.className = `hide letter ${this.phrase[i]}`;
+         }
+         ul.appendChild(li);
+       }
+     };
 
+     checkLetter(letter){
+       if(this.phrase.includes(letter)){
+         return true;
+       } else {
+         return false;
+       }
+     };
 
-// Note to self: To create a Phrase class to handle the creation of phrases.
+     showMatchedLetter(letter){
+       let selection = document.querySelectorAll(`li.hide.letter`);
+       for (let i=0; i < selection.length; i++){
+         if (selection[i].textContent === letter) {
+           selection[i].className = `show letter ${letter}`;
+         }
+       }
+     };
+}
