0c90a237aa19242807fc9895111e2ab34295fc4e
Leanne Heuberger <leanneheuberger@gmail.com>
2020-11-03 14:12:23 +0100

    Update app.js
    - Initialize game (by clicking start game button)
    - Added keyboard (onscreen + offscreen)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
-
-
- // Note to self: To create a new instance of the Game class and add event listeners for the start button and onscreen keyboard buttons.
+ /*
+  * Initialize a new game
+ */
+let game;
+document.getElementById('btn__reset').addEventListener('click', () => {
+  game = new Game;
+  game.startGame();
+});
+/*
+ * Add functionality to the onscreen keyboard buttons.
+*/
+const keyboard = document.querySelectorAll('[id=qwerty] button');
+for (const key of keyboard) {
+  key.addEventListener('click', (e) => {
+    if(e.target.className === 'key'){
+      game.handleInteraction(e.target);
+    }
+  });
+}
+/*
+ * Add functionality to the offscreen keyboard buttons.
+ * Once an offscreen button's value matches an onscreen buttons content,
+ * it follows the logic of the onscreen buttons.
+*/
+document.addEventListener('keydown', (e) => {
+  if(document.getElementById('overlay').style.display === 'none') {
+    for(let i=0; i < keyboard.length; i++){
+      if(keyboard[i].textContent === e.key){
+        game.handleInteraction(keyboard[i]);
+      }
+    }
+  }
+});
